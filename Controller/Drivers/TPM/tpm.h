976fa8ba56572f234f327d7b93e293af9b990d3d
Tommy <tommy574@protonomail.ch>
2020-11-01 20:11:56 +0200

    TPM refactoring

--- Controller/Drivers/TPM/tpm.h
+++ Controller/Drivers/TPM/tpm.h
@@ -6,13 +6,11 @@
 #pragma once
 
 /* System headers */
+#include <stdint.h>
 
 /* Device vendor headers */
-#include "MKL25Z4.h"
-//#include "fsl_bitaccess.h"
 
 /* User headers */
-#include "defines.h"
 
 /* Timings calculated from 24 MHz clock speed */
 #define MICROSECOND                         (24UL)                                      /* 1.0 s */
@@ -20,10 +18,9 @@
 #define TIME_BETWEEN_BYTES                  (6UL)                                       /* 0.25 s */
 #define TIME_PER_BYTE                       (MICROSECOND * 2 + TIME_BETWEEN_BYTES)      /* 2.25 s */
 
+#define TPM2_MICROSECONDS(x)                ((x) * MICROSECOND)
+
 /* Global function prototypes */
-void TPM0_vInit(void);
-void TPM1_vInit(void);
 void TPM2_vInit(void);
-void TPM2_vLoadCounter(uint32_t ulBytes);
 void TPM2_vStart(void);
 void TPM2_vStop(void);
