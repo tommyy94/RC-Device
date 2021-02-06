51b6dc887086234752a42b1537e1814725fe2efa
Tommy <tommy574@protonomail.ch>
2021-02-06 17:25:08 +0200

    Add TWI_xfer() implementation

--- Drivers/TWI/twi.c
+++ Drivers/TWI/twi.c
@@ -1,29 +1,57 @@
 /* Device includes */
 #include <same70q21b.h>
 #include <utils_assert.h>
+#include <stdbool.h>
 
 /* User includes */
 #include "twi.h"
 #include "pio.h"
 
-#define ADDR_MPU6050    (0x68)
+
+#define TWI0_PORT       (PIOA)
 #define PIN_TWCK0       (1 << 4u)
 #define PIN_TWD0        (1 << 3u)
+#define TWI_WRITE       (0x00u)
+#define TWI_READ        (0x01u)
 
 
-static void TWI0_IO_vInit(void);
-static void TWI_vReleaseSlave(Twihs *pxTwi);
-static void TWI_vSetMasterMode(Twihs *pxTwi);
-static void TWI_vSetSlaveMode(Twihs *pxTwi);
-static void TWI_vSendStart(Twihs *pxTwi);
-static void TWI_vSendStop(Twihs *pxTwi);
-void TWI_vWrite(Twihs *pxTwi, uint32_t ulAddr, uint8_t ucData[], const uint32_t ulLen);
+static void    TWI0_IO_vInit(void);
+static void    TWI_vReleaseSlave(Twihs *pxTwi);
+static void    TWI_vSetMasterMode(Twihs *pxTwi);
+static void    TWI_vSetSlaveMode(Twihs *pxTwi);
+static void    TWI_vWriteCR(Twihs *pxTwi, uint32_t ulMask);
+static bool    TWI_bWrite(Twihs *pxTwi, const uint32_t ulTarget, TWI_Msg *pxMsg, const uint32_t ulSr);
+static void    TWI_vRead(Twihs *pxTwi, const uint32_t ulTarget, TWI_Msg *pxMsg);
+static void    TWI_vWriteTHR(Twihs *pxTwi, const uint8_t ucByte);
+static uint8_t TWI_ucReadRHR(Twihs *pxTwi);
 
 
+/**
+ * @brief   Initialize TWI0.
+ *
+ * @param   None.
+ *
+ * @retval  None.
+ *
+ * Example:
+ *
+ *   #define ADDR_MPU6050    (0x68)
+ *   TWI_Adapter xTwiAdap;
+ *   uint8_t     buf1            = { 0x75 };
+ *   uint8_t     buf2            = { 0x00 };
+ *
+ *   xTwiAdap.pxInst             = TWIHS0;
+ *   xTwiAdap.ulAddr             = ADDR_MPU6050;
+ *   xTwiAdap.pxMsg[0].pucBuf    = &buf1;
+ *   xTwiAdap.pxMsg[0].ulLen     = 1;
+ *   xTwiAdap.pxMsg[0].ulFlags   = TWI_WRITE;
+ *   xTwiAdap.pxMsg[1].pucBuf    = &buf2;
+ *   xTwiAdap.pxMsg[1].ulLen     = 1;
+ *   xTwiAdap.pxMsg[1].ulFlags   = TWI_READ;
+ *   TWI_vXfer(&xTwiAdap, 2);
+ */
 void TWI0_vInit(void)
 {
-    uint8_t buf[3] = { 0xAA, 0xBB, 0xCC}; 
-
     TWI0_IO_vInit();
 
     /**
@@ -34,56 +62,204 @@ void TWI0_vInit(void)
     PMC->PMC_PCR |= PMC_PCR_CMD_Msk | PMC_PCR_PID(TWIHS0_CLOCK_ID) | PMC_PCR_EN_Msk;
 
     TWIHS0->TWIHS_CWGR = TWIHS_CWGR_CKDIV(1) | TWIHS_CWGR_CHDIV(43) | TWIHS_CWGR_CLDIV(43);
-    TWIHS0->TWIHS_CR |= TWIHS_CR_FIFOEN_Msk;
+    TWI_vWriteCR(TWIHS0, TWIHS_CR_FIFOEN_Msk);
 
     TWI_vSetMasterMode(TWIHS0);
-
-    TWI_vWrite(TWIHS0, ADDR_MPU6050, buf, 3);
 }
 
 
+/**
+ * @brief   Transfer messages.
+ *
+ * @param   pxAdap    Pointer to TWI adapter.
+ *
+ * @param   ulCount   Transfer count.
+ *
+ * @retval  None.
+ */
+void TWI_vXfer(TWI_Adapter *pxAdap, const uint32_t ulCount)
+{
+    /* Sanity check */
+    assert((pxAdap->pxInst == TWIHS0) || (pxAdap->pxInst == TWIHS1) || (pxAdap->pxInst == TWIHS2), __FILE__, __LINE__);
+    assert((pxAdap->pxMsg->ulFlags == TWI_WRITE) || (pxAdap->pxMsg->ulFlags == TWI_READ), __FILE__, __LINE__);
+
+    for (uint32_t ulK = 0; ulK < ulCount; ulK++)
+    {
+        if ((pxAdap->pxMsg[ulK].ulFlags == TWI_READ))
+        {
+            TWI_vRead(pxAdap->pxInst, pxAdap->ulAddr, &(pxAdap->pxMsg[ulK]));
+        }
+        else
+        {
+            if (TWI_bWrite(pxAdap->pxInst, pxAdap->ulAddr, &(pxAdap->pxMsg[ulK]), ulK) == false)
+            {
+                TWI_vReleaseSlave(pxAdap->pxInst);
+            }
+        }
+    }
+
+    while ((pxAdap->pxInst->TWIHS_SR & TWIHS_SR_TXCOMP_Msk) == 0)
+    {
+        ; /* Wait until transmission complete */
+    }
+}
+
+
+/**
+ * @brief   Configure TWI0 pins as peripheral function and
+ *          enable internal pullups.
+ *
+ * @param   None.
+ *
+ * @retval  None.
+ */
 static void TWI0_IO_vInit(void)
 {
-    PIO_ConfigurePull(PIOA, PIN_TWCK0 | PIN_TWD0, PIO_PULLUP);
-    PIO_vSetPeripheralFunction(PIOA, PIN_TWCK0 | PIN_TWD0, PIO_PERIPH_A);
+    PIO_ConfigurePull(TWI0_PORT, PIN_TWCK0 | PIN_TWD0, PIO_PULLUP);
+    PIO_vSetPeripheralFunction(TWI0_PORT, PIN_TWCK0 | PIN_TWD0, PIO_PERIPH_A);
 }
 
 
-void TWI_vWrite(Twihs *pxTwi, uint32_t ulAddr, uint8_t ucData[], const uint32_t ulLen)
+/**
+ * @brief   Write Transmit Holding Register.
+ *
+ * @param   pxTwi     TWI instance pointer.
+ *
+ * @param   ucByte    Write value.
+ */
+static void TWI_vWriteTHR(Twihs *pxTwi, const uint8_t ucByte)
 {
-    assert(ulAddr <= 0x7F, __FILE__, __LINE__);
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
 
-    pxTwi->TWIHS_MMR |= TWIHS_MMR_DADR(ulAddr) & ~TWIHS_MMR_MREAD_Msk;
+    pxTwi->TWIHS_THR = TWIHS_THR_TXDATA(ucByte);
+    while ((pxTwi->TWIHS_SR & TWIHS_SR_TXRDY_Msk) == 0)
+    {
+        ; /* Wait until data sent */
+    }
+}
+
+
+/**
+ * @brief   Read Receive Holding Register.
+ *
+ * @param   pxTwi     TWI instance pointer.
+ *
+ * @retval  RHR       Register value.
+ */
+static uint8_t TWI_ucReadRHR(Twihs *pxTwi)
+{
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
+
+    while ((pxTwi->TWIHS_SR & TWIHS_SR_RXRDY_Msk) == 0)
+    {
+        ; /* Wait until receiver ready */
+    }
+    return pxTwi->TWIHS_RHR;
+}
+
+
+/**
+ * @brief   Write TWI.
+ *
+ * @param   pxTwi     TWI instance pointer.
+ *
+ * @param   ulTarget  Device address to write.
+ *
+ * @param   pxMsg     Message buffer.
+ *
+ * @param   ulSr      Repeated Start.
+ *
+ * @retval  bRet      Write success/failure.
+ */
+static bool TWI_bWrite(Twihs *pxTwi, const uint32_t ulTarget, TWI_Msg *pxMsg, const uint32_t ulSr)
+{
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
+
+    bool bRet = true;
+
+    pxTwi->TWIHS_MMR |= TWIHS_MMR_DADR(ulTarget) & ~TWIHS_MMR_MREAD_Msk;
     __DMB();
 
     /* START bit sent automatically when writing */
-    for (uint32_t ulCnt = 0; ulCnt < ulLen; ulCnt++)
+    for (uint32_t ulCnt = 0; ulCnt < pxMsg->ulLen; ulCnt++)
     {
-        pxTwi->TWIHS_THR = TWIHS_THR_TXDATA(ucData[ulCnt]);
-        while ((pxTwi->TWIHS_SR & TWIHS_SR_TXRDY_Msk) == 0)
-        {
-            ; /* Wait until data sent */
-        }
+        TWI_vWriteTHR(pxTwi, pxMsg->pucBuf[ulCnt]);
 
         /*  Check if ACK received */
         if ((pxTwi->TWIHS_SR & TWIHS_SR_NACK_Msk) != 0)
         {
-            __BKPT();
+            bRet = false;
         }
     }
 
-    TWI_vSendStop(pxTwi);
-
-    while ((pxTwi->TWIHS_SR & TWIHS_SR_TXCOMP_Msk) == 0)
+    /* Send STOP bit if last xfer */
+    if (ulSr <= 1)
     {
+        TWI_vWriteCR(pxTwi, TWIHS_CR_STOP_Msk);
+    }
 
+    return bRet;
+}
+
+
+/**
+ * @brief   Read TWI.
+ *
+ * @param   pxTwi     TWI instance pointer.
+ *
+ * @param   ulTarget  Device address to read.
+ *
+ * @param   pxMsg     Receive buffer.
+ *
+ * @retval  None.
+ */
+static void TWI_vRead(Twihs *pxTwi, const uint32_t ulTarget, TWI_Msg *pxMsg)
+{
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
+
+    uint32_t ulCnt = 0;
+
+    /* START & STOP on single byte read */
+    if (pxMsg->ulLen == 1)
+    {
+        pxTwi->TWIHS_MMR |= TWIHS_MMR_DADR(ulTarget) | TWIHS_MMR_MREAD_Msk;
+        __DMB();
+        TWI_vWriteCR(pxTwi, TWIHS_CR_START_Msk | TWIHS_CR_STOP_Msk);
+        pxMsg->pucBuf[ulCnt] = TWI_ucReadRHR(pxTwi);
+    }
+    else
+    {
+        pxTwi->TWIHS_MMR |= TWIHS_MMR_DADR(ulTarget) | TWIHS_MMR_MREAD_Msk;
+        __DMB();
+        TWI_vWriteCR(pxTwi, TWIHS_CR_START_Msk);
+        for (ulCnt = ulCnt; ulCnt < (pxMsg->ulLen - 1); ulCnt++)
+        {
+            pxMsg->pucBuf[ulCnt] = TWI_ucReadRHR(pxTwi);
+        }
+
+        /* Send STOP before reading the last byte to avoid
+         * initiating a unnecessary transaction
+         */
+        TWI_vWriteCR(pxTwi, TWIHS_CR_STOP_Msk);
+        pxMsg->pucBuf[ulCnt] = TWI_ucReadRHR(pxTwi);
     }
 }
 
 
-void TWI_vRead(Twihs *pxTwi)
+/**
+ * @brief   Write control register.
+ *
+ * @param   pxAdap    Pointer to TWI adapter.
+ *
+ * @param   ulMask    Mask to write.
+ *
+ * @retval  None.
+ */
+static void TWI_vWriteCR(Twihs *pxTwi, uint32_t ulMask)
 {
-    TWIHS0->TWIHS_MMR |= TWIHS_MMR_MREAD_Msk;
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
+    pxTwi->TWIHS_CR = ulMask;
+    __DMB();
 }
 
 
@@ -96,36 +272,35 @@ void TWI_vRead(Twihs *pxTwi)
  */
 static void TWI_vReleaseSlave(Twihs *pxTwi)
 {
-    /* Configure SCK as GPIO */
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
 
-    for (uint32_t ulPulse = 0; ulPulse < 9; ulPulse++)
-    {
-        /* Toggle IO lines to generate pulses */
-    }
-
-    /* Configure SCK as peripheral function */
+    pxTwi->TWIHS_CR = TWIHS_CR_CLEAR_Msk;
+    __DMB();
 }
 
-
+/**
+ * @brief   Set TWI master mode.
+ *
+ * @param   pxTwi   Pointer to TWI instance.
+ *
+ * @retval  None.
+ */
 static void TWI_vSetMasterMode(Twihs *pxTwi)
 {
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
     pxTwi->TWIHS_CR = TWIHS_CR_MSEN_Msk | TWIHS_CR_SVDIS_Msk;
 }
 
 
+/**
+ * @brief   Set TWI slave mode.
+ *
+ * @param   pxTwi   Pointer to TWI instance.
+ *
+ * @retval  None.
+ */
 static void TWI_vSetSlaveMode(Twihs *pxTwi)
 {
+    assert((pxTwi == TWIHS0) || (pxTwi == TWIHS1) || (pxTwi == TWIHS2), __FILE__, __LINE__);
     pxTwi->TWIHS_CR = TWIHS_CR_MSDIS_Msk | TWIHS_CR_SVEN_Msk;
-}
-
-
-static void TWI_vSendStart(Twihs *pxTwi)
-{
-    pxTwi->TWIHS_CR = TWIHS_CR_START_Msk;
-}
-
-
-static void TWI_vSendStop(Twihs *pxTwi)
-{
-    pxTwi->TWIHS_CR = TWIHS_CR_STOP_Msk;
 }
--- Drivers/TWI/twi.h
+++ Drivers/TWI/twi.h
@@ -1,3 +1,23 @@
 #pragma once
 
+
+#define TWI_MSG_LIMIT   (2u)
+
+
+typedef struct
+{
+    uint8_t     *pucBuf;
+    uint32_t     ulLen;
+    uint32_t     ulFlags;
+} TWI_Msg;
+
+typedef struct
+{
+    Twihs           *pxInst;
+    uint32_t         ulAddr;
+    TWI_Msg          pxMsg[TWI_MSG_LIMIT];
+} TWI_Adapter;
+
+
 void TWI0_vInit(void);
+void TWI_vXfer(TWI_Adapter *pxAdap, const uint32_t ulCount);
